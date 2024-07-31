25cb3e8a6950484f792e5f419b4b5e800fd5986e
Nishevitha <nishevitha.v@gmail.com>
2024-07-31 18:13:57 +0800

    completed input field

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,13 @@
 import bounce from './bounce.svg'
 import logo from './logo.svg'
+import clipboard from './clipboard.svg'
+import url from './url.svg'
+import enter from './enter.svg'
 
 export {
     bounce,
-    logo
+    logo, 
+    clipboard, 
+    url, 
+    enter
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { bounce, clipboard, enter, url } from '../../assets'
 import './Main.css'
 
 const Main = () => {
   return (
-    <div></div>
+    <main className="summarizer__main">
+      
+      <div className="summarizer__main-search">
+        <form className="summarizer__main-search__bar" onSubmit={() => {}}>
+          <img src={url} alt="url" className="summarizer__main-search__bar-link" />
+
+          <input type="url" placeholder="https://www.example.com" value="" onChange={() => {}} required className="summarizer__main-search__bar-input" />
+          <button type="submit" className="summarizer__main-search__bar-button">
+            <img src={enter} alt="submit" />
+          </button>
+        </form>
+      </div>
+
+
+    </main>
   )
 }
 
